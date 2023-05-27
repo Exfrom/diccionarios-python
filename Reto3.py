e7a87627191f1c706eb40d11abba9f3c03dfe638
Exfrom <dscelis@soy.sena.edu.co>
2023-05-27 16:01:15 -0500

    se corrige ejercicio

--- Reto3.py
+++ Reto3.py
@@ -1,69 +1,80 @@
 instructores2557861 = {}
 
-def agregar_modificar():
-    nombre = input("Ingrese el nombre del instructor: ")
-    if nombre in instructores2557861:
-        print("Nombre:", nombre)
-        print("Materia:", instructores2557861[nombre]["materia"])
-        print("Teléfono:", instructores2557861[nombre]["telefono"])
-        modificar = input("¿Desea modificar la materia o el teléfono? (s/n): ")
-        if modificar.lower() == "s":
-            materia = input("Ingrese la materia que dicta el instructor: ")
-            telefono = input("Ingrese el número de teléfono del instructor: ")
-            instructores2557861[nombre]["materia"] = materia
-            instructores2557861[nombre]["telefono"] = telefono
-            print("Los datos del instructor han sido actualizados.")
-    else:
-        materia = input("Ingrese la materia que dicta el instructor: ")
-        telefono = input("Ingrese el número de teléfono del instructor: ")
-        instructores2557861[nombre] = {"materia": materia, "telefono": telefono}
-        print("El instructor ha sido agregado a la lista.")
-
-def buscar():
-    texto_busqueda = input("Ingrese el texto de búsqueda: ")
-    for nombre in instructores2557861:
-        if nombre.lower().startswith(texto_busqueda.lower()):
-            print("Nombre:", nombre)
-            print("Materia:", instructores2557861[nombre]["materia"])
-            print("Teléfono:", instructores2557861[nombre]["telefono"])
-
-def borrar():
-    nombre = input("Ingrese el nombre del instructor que desea borrar: ")
-    if nombre in instructores2557861:
-        confirmar = input("¿Está seguro que desea borrar a {} de la agenda? (s/n): ".format(nombre))
-        if confirmar.lower() == "s":
-            del instructores2557861[nombre]
-            print("El instructor ha sido eliminado de la lista.")
-    else:
-        print("El instructor no se encuentra en la lista.")
-
-def listar():
-    print("Lista de instructores:")
-    for nombre in instructores2557861:
-        print("Nombre:", nombre)
-        print("Materia:", instructores2557861[nombre]["materia"])
-        print("Teléfono:", instructores2557861[nombre]["telefono"])
-        print()
-
 while True:
-    print("\nMenú:")
+    print("Agenda de Instructores")
     print("1. Agregar/Modificar")
     print("2. Buscar")
     print("3. Borrar")
     print("4. Listar")
     print("5. Salir")
-    opcion = input("Ingrese una opción: ")
 
-    if opcion == "1":
-        agregar_modificar()
-    elif opcion == "2":
-        buscar()
-    elif opcion == "3":
-        borrar()
-    elif opcion == "4":
-        listar()
-    elif opcion == "5":
-        print("Saliendo del programa... Adios!")
+    opcion = int(input("¿Qué opción desea realizar?: "))
+
+    if opcion == 1:
+        nombre = input("Ingrese el nombre del instructor: ")
+        
+        if nombre in instructores2557861:
+            print(f"Instructor: {nombre}")
+            print(f"Materia: {instructores2557861[nombre]['materia']}")
+            print(f"Teléfono: {instructores2557861[nombre]['telefono']}")
+            
+            modificar = input("¿Desea modificar los datos? (s/n): ")
+            if modificar.lower() == "s":
+                materia = input("Ingrese la materia: ")
+                telefono = input("Ingrese el teléfono: ")
+                instructores2557861[nombre]['materia'] = materia
+                instructores2557861[nombre]['telefono'] = telefono
+                print("Datos del instructor modificados correctamente.")
+
+        else:
+            materia = input("Ingrese la materia: ")
+            telefono = input("Ingrese el teléfono: ")
+            instructores2557861[nombre] = {'materia': materia, 'telefono': telefono}
+            print("Instructor agregado correctamente.")
+
+    elif opcion == 2:
+        texto = input("Ingrese un texto de búsqueda: ")
+        encontrados = []
+
+        for nombre, datos in instructores2557861.items():
+            if nombre.startswith(texto):
+                encontrados.append(nombre)
+
+        if len(encontrados) > 0:
+            print("Instructores encontrados:")
+            for nombre in encontrados:
+                print(f"Instructor: {nombre}")
+                print(f"Materia: {instructores2557861[nombre]['materia']}")
+                print(f"Teléfono: {instructores2557861[nombre]['telefono']}")
+                print("--------------")
+        else:
+            print("No se encontraron instructores con ese nombre.")
+
+    elif opcion == 3:
+        nombre = input("Ingrese el nombre del instructor a borrar: ")
+
+        if nombre in instructores2557861:
+            borrar = input(f"¿Está seguro de que desea borrar a '{nombre}'? (s/n): ")
+            if borrar.lower() == "s":
+                del instructores2557861[nombre]
+                print("Instructor borrado correctamente.")
+        else:
+            print("El instructor no existe en la agenda.")
+
+    elif opcion == 4:
+        print("Lista de instructores:")
+        if len(instructores2557861) > 0:
+            for nombre, datos in instructores2557861.items():
+                print(f"Instructor: {nombre}")
+                print(f"Materia: {datos['materia']}")
+                print(f"Teléfono: {datos['telefono']}")
+                print("--------------")
+        else:
+            print("La agenda de instructores está vacía.")
+
+    elif opcion == 5:
+        print("Saliendo del programa... ¡Adiós!")
         break
+
     else:
-        print("Opción inválida.")
+        print("Opción inválida. Por favor, seleccione una opción válida.")
